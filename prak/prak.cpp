0691030e0726c8b74f0a150bbc9d6acb8c78284f
Asti22 <uun3461@gmail.com>
2024-03-07 17:18:04 +0700

    menampilkan element array

--- prak/prak.cpp
+++ prak/prak.cpp
@@ -20,7 +20,7 @@ void input() {
 	}
 	cout << endl;
 	cout << "===============" << endl;
-	cout << "Masukan elemen array" << endl;
+	cout << "Masukan Element Array" << endl;
 	cout << "===============" << endl;
 
 	for (int i = 0; i < n; i++) {

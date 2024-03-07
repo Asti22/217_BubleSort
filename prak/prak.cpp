8d723c1d8c6f4384da718dc377908ddcb1301e56
Asti22 <uun3461@gmail.com>
2024-03-07 15:30:03 +0700

    Add project files.

---
+++ prak/prak.cpp
@@ -0,0 +1,18 @@
+// prak.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std; 
+
+int arr[20];
+int n;
+
+void input()
+{
+    while (true) {
+    cout << ""
+    }
+    
+}
+
+
